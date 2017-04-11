d1cfcc50ccf2c9daa15dfc3c5ed89d82d6dd99b5
github username <github email address>
2017-04-11 17:42:22 -0400

    Done.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,23 @@
+class MyValidator < ActiveModel::Validator
+
+  def validate(post)
+    	unless post.title =~ /Won't Believe|Secret|Top \d|Guess/
+    		post.errors[:title] << 'Blah'
+    	end
+    
+  end
+end
+
+
+
+
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :content, length: {minimum:250}
+	validates :summary, length: {maximum:250}
+	validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+	
+	include ActiveModel::Validations
+  validates_with MyValidator
+
 end
